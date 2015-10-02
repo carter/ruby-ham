83a2dba923e3833bf455f4ed0968c4e70fa3f33c
Carter Parks <carter@carterparks.com>
2015-10-01 22:13:49 -0600

    only update once an hour

--- n0nbh.rb
+++ n0nbh.rb
@@ -2,7 +2,7 @@ require 'open-uri'
 require 'nokogiri'
 
 class N0nbh
-  @@xml_doc 
+  @@xml_doc = nil
   @@url = "http://www.hamqsl.com/solarxml.php"
   def self.band_conditions(band, time)
     xml.at_css("calculatedconditions band[name='#{band}'][time='#{time}']").inner_html
@@ -13,6 +13,21 @@ class N0nbh
   end
 
   def self.xml
-    @@xml_doc ||= Nokogiri::HTML(open(@@url))
+    if needs_update?
+      @@xml_doc = Nokogiri::HTML(open(@@url))
+    else
+      @@xml_doc
+    end
+  end
+
+  def self.last_update
+    time_string = @@xml_doc.at_css('updated').inner_html.strip
+    format = "%d %b %Y %H%M %Z"
+    return Time.strptime(time_string,format)
+  end
+
+  def self.needs_update?
+    return true if !@@xml_doc
+    Time.now - last_update > 3600
   end
 end
--- spec/n0nbh_spec.rb
+++ spec/n0nbh_spec.rb
@@ -11,7 +11,7 @@ VCR.configure do |config|
 end
 
 RSpec.describe N0nbh do
-  use_vcr_cassette "solar_conditions"
+  use_vcr_cassette "solar_conditions", :allow_playback_repeats => true
   
   describe "#band_conditions" do
     it "should return an indication of the band conditions" do
@@ -23,6 +23,19 @@ RSpec.describe N0nbh do
   describe "#[]" do
     it "should return the value in the brackets" do
       expect(N0nbh['solarflux']).to eq("114")
+    end
+  end
+
+  describe "#last_update" do
+    it "should return the time of the last update" do
+      skip
+      expect(N0nbh.last_update).to eq('fdsa') 
+    end
+  end
+
+  describe "#needs_update" do
+    it "should return false" do
+      expect(N0nbh.needs_update?).to be true
     end
   end
 end
