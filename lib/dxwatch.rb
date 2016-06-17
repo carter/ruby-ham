3f8929c9342ae410203217746a42e6ed2f7fdca9
Carter Parks <carter@carterparks.com>
2016-06-16 18:04:27 -0600

    fixed rbn

--- lib/dxwatch.rb
+++ lib/dxwatch.rb
@@ -9,28 +9,49 @@ class DXWatch
     rbn_url = "http://www.reversebeacon.net/dxsd1/sk.php?s=0&r=15&cdx=#{callsign}"
     rbn_json = JSON.load(open(rbn_url))
 
-    spots = {}
-    spots.merge!(dxw_json['s']) if dxw_json['s']
-    spots.merge!(rbn_json['s']) if rbn_json['s']
-
     data = []
-    spots.each_pair do |key, v|
-      time = v[4]
-      hour = time[0..1]
-      minute = time[2..3]
-      date = time.match(/(.*)z (.*)/)[2]
-      parsed = DateTime.parse("#{hour}:#{minute} UTC #{date}").to_time
-      next if parsed > Time.now
-      data << {
-        id: key,
-        de: v[0],
-        frequency: v[1],
-        dx: v[2],
-        comment: v[3],
-        time: parsed,
-        original_time: v[4]
+    if dxw_json['s']
+      dxw_json['s'].each_pair do |key, v|
+        time = v[4]
+        hour = time[0..1]
+        minute = time[2..3]
+        date = time.match(/(.*)z (.*)/)[2]
+        parsed = DateTime.parse("#{hour}:#{minute} utc #{date}").to_time
+        next if parsed > Time.now
+        data << {
+          id: key,
+          de: v[0],
+          frequency: v[1],
+          dx: v[2],
+          comment: v[3],
+          time: parsed,
+          original_time: v[4],
+          on: "DX Watch"
+        }
+      end
+    end
+    if rbn_json['s']
+      rbn_json['s'].each_pair do |key, v|
+        time = v[5]
+        hour = time[0..1]
+        minute = time[2..3]
+        date = time.match(/(.*)z (.*)/)[2]
+        parsed = DateTime.parse("#{hour}:#{minute} utc #{date}").to_time
+        next if parsed > Time.now
+        data << {
+          id: key,
+          de: v[0],
+          frequency: v[1],
+          dx: v[2],
+          db: v[3],
+          wpm: v[4],
+          comment: "#{v[3]}dB #{v[4]}wpm",
+          time: parsed,
+          original_time: v[5], 
+          on: 'RBN'
 
-      }
+        }
+      end
     end
     return data.sort {|y, x| x[:time] <=> y[:time] }
   end
